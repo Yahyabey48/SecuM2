a839e4eca91e18072f35ab97f80c85879dec6ae6
Sam-esgi <115066347+Sam-esgi@users.noreply.github.com>
2024-11-08 12:30:11 +0100

    Add files via upload
    Ajout fichier variables

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  type    = string
+  default = "eu-west-3"
+}
+
+variable "mail" {
+  type    = string
+  default = "sdillmann@myges.fr"
+}
+
+variable "ip_vpc" {
+  type    = string
+  default = "10.80.0.0/16"
+}
+
+variable "ip_subnet" {
+  type    = string
+  default = "10.80.1.0/24"
+}
+
+variable "ip_all" {
+  type    = string
+  default = "0.0.0.0/0"
+}
